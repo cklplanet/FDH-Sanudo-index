ba5c3603ebbe12af1d942afa474ba97439fc733d
Bedardy <86094513+Bedardy@users.noreply.github.com>
2024-12-05 10:45:00 +0100

    italian name extraction function

--- name_extraction_from_paragraph.py
+++ name_extraction_from_paragraph.py
@@ -2,31 +2,35 @@
 from transformers import DistilBertTokenizerFast, DistilBertForTokenClassification
 from transformers import pipeline
 
-tokenizer = DistilBertTokenizerFast.from_pretrained("osiria/distilbert-italian-cased-ner")
-model = DistilBertForTokenClassification.from_pretrained("osiria/distilbert-italian-cased-ner")
 
-ner = pipeline("ner", model = model, tokenizer = tokenizer, aggregation_strategy="first")
+def italian_name_extraction(text):
+    tokenizer = DistilBertTokenizerFast.from_pretrained("osiria/distilbert-italian-cased-ner")
+    model = DistilBertForTokenClassification.from_pretrained("osiria/distilbert-italian-cased-ner")
+    ner = pipeline("ner", model = model, tokenizer = tokenizer, aggregation_strategy="first")
+    results = ner(text)
+    person_names = list({entity['word'] for entity in results if entity['entity_group'] == 'PER'})
+    # Output the list of unique person names
+    return person_names
+
+# Example usage
+
 
 # The recognition of the GPT-corrected text segments is dramatically improved.
 # Segment 676, GPT corrected version
-text = '''
-Præscriptum, ut hæ nuptiæ tali auspicio sint insulæ  
-ac stipulate, ut conjugibus ipsis contra regnis et populis sint usui et voluptati, et ceterisque Christianis omnibus pariant et confirmet perpetuam pacem et tranquillitatem. 
 
-Serenissimo Hungariæ Regi.
+# text = '''
+# Præscriptum, ut hæ nuptiæ tali auspicio sint insulæ  
+# ac stipulate, ut conjugibus ipsis contra regnis et populis sint usui et voluptati, et ceterisque Christianis omnibus pariant et confirmet perpetuam pacem et tranquillitatem. 
 
-Ingentem animo concepimus betitiarum, obi primum ex oratoris istius nostris litteris proxime bih divimus nuptias inter Regem Gelisito et serenissimam Mariam Cesaree et Catholicæ Majestatis sororem interque illustrissimum Ferdinandum, ejusdem unicum fratrem et Illustrissimam Bai Annam Majestatis Vestræ sororem auspicatissimum basim celebratas; nam si quas mutua duo ista connubia duosque reges ac principes potentissimos præclarissimis ortis natalibus tam arctissimo nexu copulatos animo voluerit, si sponsas sponsosque etiam virentes et interea ad transigendum hoc auspiciatum connubium, ad procurandam regiam nobiliorem maxime aptos, si fructus denique elæconomos que Christianis omnibus inde proficere diligentius considerarit, nihil . . . reperiet quod sit hoc vere regio matrimonio vel nobilibus vel præclaris vel majore laude dignius. Quare factum est ut per ea quæ semper Serenissimæ Majestatis nostræ progenitores hereditario quodam jure prosecuto sumus, et præsentibus eadem prosequimur benevolentia et observatione. Nihil prius aut antiquius simus arbitri, quod bis nostris litteris eidem Majestati Vestræ etiam atque etiam summopere gratulati, atque eo quod in spe fore ut Majestatis Vostra novo isto adiuncto conjugio, quod fortunatissimum esse et copiosum et optandum, Cesareæ et Catholicæ Majestatis (ut par est) Consilio auctoritate et viribus adiutis, de universa Christianæ religione fidas poterit promoveri.
+# Serenissimo Hungariæ Regi.
 
-Serenissimo Regi Ferdinando archiduci Austrie infantis Hispaniarum etc.
+# Ingentem animo concepimus betitiarum, obi primum ex oratoris istius nostris litteris proxime bih divimus nuptias inter Regem Gelisito et serenissimam Mariam Cesaree et Catholicæ Majestatis sororem interque illustrissimum Ferdinandum, ejusdem unicum fratrem et Illustrissimam Bai Annam Majestatis Vestræ sororem auspicatissimum basim celebratas; nam si quas mutua duo ista connubia duosque reges ac principes potentissimos præclarissimis ortis natalibus tam arctissimo nexu copulatos animo voluerit, si sponsas sponsosque etiam virentes et interea ad transigendum hoc auspiciatum connubium, ad procurandam regiam nobiliorem maxime aptos, si fructus denique elæconomos que Christianis omnibus inde proficere diligentius considerarit, nihil . . . reperiet quod sit hoc vere regio matrimonio vel nobilibus vel præclaris vel majore laude dignius. Quare factum est ut per ea quæ semper Serenissimæ Majestatis nostræ progenitores hereditario quodam jure prosecuto sumus, et præsentibus eadem prosequimur benevolentia et observatione. Nihil prius aut antiquius simus arbitri, quod bis nostris litteris eidem Majestati Vestræ etiam atque etiam summopere gratulati, atque eo quod in spe fore ut Majestatis Vostra novo isto adiuncto conjugio, quod fortunatissimum esse et copiosum et optandum, Cesareæ et Catholicæ Majestatis (ut par est) Consilio auctoritate et viribus adiutis, de universa Christianæ religione fidas poterit promoveri.
 
-Postea quam vobis nuperrime nunciavimus Majestatem Vestram coniunctam esse serenissimæ Anne, serenissimi Hungariæ regis sorori, omnibus speciosissime, itidemque serenissime ille rex Majestatis Vestræ sororem in uxorem accepisse, certe pro nostra erga Catholicam et Cesaream Majestatem perpetua observantia, pariterque erga Majestatem Vestram sincere nostro benevolo spiritu.
-'''
+# Serenissimo Regi Ferdinando archiduci Austrie infantis Hispaniarum etc.
 
-results = ner(text)
-
-person_names = list({entity['word'] for entity in results if entity['entity_group'] == 'PER'})
-# Output the list of unique person names
-print(person_names)
+# Postea quam vobis nuperrime nunciavimus Majestatem Vestram coniunctam esse serenissimæ Anne, serenissimi Hungariæ regis sorori, omnibus speciosissime, itidemque serenissime ille rex Majestatis Vestræ sororem in uxorem accepisse, certe pro nostra erga Catholicam et Cesaream Majestatem perpetua observantia, pariterque erga Majestatem Vestram sincere nostro benevolo spiritu.
+# '''
+# print(italian_name_extraction(text))
 
 # # Can also print other name entitys
 # location_names = list({entity['word'] for entity in results if entity['entity_group'] == 'LOC'})
